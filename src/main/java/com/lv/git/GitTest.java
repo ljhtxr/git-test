82b564763975fe7666fd60c7cd44314c81124f31
Ljh <Ljh@jiaheng.com>
2022-08-27 00:13:25 +0800

    first commit

---
+++ src/main/java/com/lv/git/GitTest.java
@@ -0,0 +1,11 @@
+package com.lv.git;
+
+/**
+ * @author lvstart
+ * @date 2022/8/27 - 0:09
+ */
+public class GitTest {
+    public static void main(String[] args) {
+        System.out.println("hello git!");
+    }
+}
